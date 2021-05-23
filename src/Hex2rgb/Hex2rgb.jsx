757711aa0b2e00aacd82b7a6fe49f5aff78c40fc
andrei-pelevin <pelevin72@gmail.com>
2021-05-23 12:10:47 +0300

    correction

--- src/Hex2rgb/Hex2rgb.jsx
+++ src/Hex2rgb/Hex2rgb.jsx
@@ -8,8 +8,8 @@ export default function Hex2rgb() {
             hex = hex.slice(1);
         }
         
-
         var digit = hex.split("");
+
         if (digit.length === 3) {
             digit = [digit[0], digit[0], digit[1], digit[1], digit[2], digit[2]]
         }
@@ -17,33 +17,43 @@ export default function Hex2rgb() {
         var g = parseInt([digit[2], digit[3]].join(""), 16);
         var b = parseInt([digit[4], digit[5]].join(""), 16);
 
+        setForm(prevForm => ({ ...prevForm, result: `rgb(${r}, ${g}, ${b})`, color: `rgb(${r}, ${g}, ${b})` }));        
 
-        setForm(prevForm => ({ ...prevForm, result: `rgb(${r}, ${g}, ${b})`, color: `rgb(${r}, ${g}, ${b})` }))
-
-        if (isNaN(r) || isNaN(g) || isNaN(b)) {
-            setForm(prevForm => ({ ...prevForm, result: `Ошибка!`, color: 'red' }))
-        }
+        digit.map(item => {
+            if(isNaN(parseInt(item, 16))) {
+                setForm(prevForm => ({ ...prevForm, result: `Ошибка!`, color: 'red' }))
+            }
+           return ''
+        })
         return `rgb(${r}, ${g}, ${b})`;
     }
 
-    const [form, setForm] = useState({ value: '#' })
+    const [form, setForm] = useState({ value: '#' });
 
 
     const handleSubmit = (event) => {
         event.preventDefault();
         hex(form.value);
-    }
+    };
 
 
     function handleColorChange(event) {
-        const color = event.target.value
+        let color = event.target.value
         setForm(prevForm => ({ ...prevForm, value: color }))
 
         if (color.length === 7) {
             hex(color);
             
-        }
-    }
+        } else if (color.length > 7) {
+            color = color.slice(0, -1);
+            setForm(prevForm => ({ ...prevForm, value: color }))
+        } else if (color.length === 0) {
+            color = '#';
+            setForm(prevForm => ({ ...prevForm, value: color }))
+        } 
+
+        
+    };
 
 
 
